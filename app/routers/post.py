f7b969345ce050a7f92e2ed8769cd54aaf5001b4
Harsh <harshrawat096@gmail.com>
2023-04-14 07:06:47 +0530

    Add query parameter for good searching of post

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,12 +14,14 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schema.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit:int=10, skip:int=0, search: Optional[str] = ""):
     # Sql
     # cursor.execute("""Select * from posts")
     # post = cursor.fetchall()
 
-    post = db.query(models.Post).all()
+    # post = db.query(models.Post).all()
+
+    post = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     
     
     # If you want to reterive only login user posts
