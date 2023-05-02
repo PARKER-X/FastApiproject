891e5c93095d573828a99a72b3e0b3341a3f1bd2
Harsh <harshrawat096@gmail.com>
2023-05-03 04:57:08 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -5,13 +5,9 @@ from .routers import post, user, auth, vote
 from .config import settings
 
 
-
-
 models.Base.metadata.create_all(bind=engine)
 
-
 app = FastAPI()
-
 
 app.include_router(post.router)
 app.include_router(user.router)
--- app/oauth2.py
+++ app/oauth2.py
@@ -30,7 +30,6 @@ def create_access_token(data:dict):
     return encoded_jwt
 
 
-
 def verify_access_token(token:str, credentials_exception):
     try:
         payload = jwt.decode(token,SECRET_KEY, algorithms=[ALGORITHM])
